97ee9b582a77e4d066922721441245b90a0f23e7
Piotr Janik <janikpiotrek@gmail.com>
2014-02-04 20:58:38 +0100

    Add IFramePhone code extracted from Lab repository (+ refactor it)

---
+++ main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  /**
+   * Allows to communicate with an iframe.
+   */
+  ParentEndpoint:  require('./lib/parent-endpoint'),
+  /**
+   * Allows to communicate with a parent page.
+   * IFrameEndpoint is a singleton, as iframe can't have multiple parents anyway.
+   */
+  getIFrameEndpoint: require('./lib/iframe-endpoint'),
+  structuredClone: require('./lib/structured-clone'),
+};
